5fedacb57d81b59bb91889706b7f9c7fcbb4d7f3
keklick1337 <86933883+keklick1337@users.noreply.github.com>
2025-01-04 21:42:25 +0400

    fix cross_build on Mac M1

--- cross-build/cross_build.sh
+++ cross-build/cross_build.sh
@@ -2,10 +2,10 @@
 CROSS_BUILD_NAME="phantomgate-build-cross"
 I386_BUILD_NAME="phantomgate-build-i386"
 
-docker build -t $CROSS_BUILD_NAME -f Dockerfile.cross .
+docker build --platform linux/amd64 -t $CROSS_BUILD_NAME -f Dockerfile.cross .
 docker build --platform linux/amd64 -t $I386_BUILD_NAME -f Dockerfile.i386 .
 
-rm -rf aarch64 armv7 mips x86_64 riscv64 i386
+rm -rf aarch64 armv7 mips x86_64 riscv64 i386 *.tar.gz
 
 docker run --rm -v "$PWD/../src":/src -v "$PWD":/cross-build -v "$PWD/../signatures.txt":/signatures.txt $CROSS_BUILD_NAME
 docker run --rm -v "$PWD/../src":/src -v "$PWD":/cross-build -v "$PWD/../signatures.txt":/signatures.txt $I386_BUILD_NAME
